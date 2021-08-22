d308666c8d4a18df4101bee886a139fa6338fb97
wyp0712 <31912068+wyp0712@users.noreply.github.com>
2021-08-22 19:09:12 +0800

    add: home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,44 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Switch, NavLink, HashRouter } from "react-router-dom";
+
+const HeaderBar = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>部署项目</h1>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+};
+
+const About = () => {
+  return (
+    <header className="App-header">
+      <h1>About</h1>
+    </header>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>部署项目</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <NavLink to="/">首页</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <Switch>
+          <Route exact path="/" component={HeaderBar}></Route>
+          <Route path="/about" component={About}></Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
